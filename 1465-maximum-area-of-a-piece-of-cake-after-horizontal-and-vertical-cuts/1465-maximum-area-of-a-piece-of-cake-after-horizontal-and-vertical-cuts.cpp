f4bde9ca5d15603d6f7e04f46efaed1fe3b87ee9
Chanda Gupta <chandaincse801@gmail.com>
2022-07-02 16:56:48 +0530

    Time: 79 ms (92.25%), Space: 32.4 MB (13.28%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
     int maxArea(int h, int w, vector<int>& horizontalCuts, vector<int>& verticalCuts) {
+        horizontalCuts.push_back(h);
+        verticalCuts.push_back(w);
+        sort(horizontalCuts.begin(), horizontalCuts.end());
+        sort(verticalCuts.begin(), verticalCuts.end());
+        int hr = horizontalCuts[0], vr = verticalCuts[0];
+        int m = 1e9+7;
+        for(int i=1;i<horizontalCuts.size();i++){
+            hr = max(hr, horizontalCuts[i] - horizontalCuts[i-1]);
+        }
+        for(int i=1;i<verticalCuts.size();i++){
+            vr = max(vr, verticalCuts[i] - verticalCuts[i-1]);
+        }
+        return (1LL*hr*vr)%m;
+    }
+    /*
+    int maxArea(int h, int w, vector<int>& horizontalCuts, vector<int>& verticalCuts) {
         sort(horizontalCuts.begin(), horizontalCuts.end());
         sort(verticalCuts.begin(), verticalCuts.end());
         long hr = horizontalCuts[0], vr = verticalCuts[0];
@@ -19,4 +35,5 @@ public:
         vr = max(vr, t);
         return (hr%m*vr%m)%m;
     }
+    */
 };
