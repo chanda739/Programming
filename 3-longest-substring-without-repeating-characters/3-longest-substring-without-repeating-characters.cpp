ecc3eed9cd8806b2ca9dc9904f3f08378cad730d
Chanda Gupta <chandaincse801@gmail.com>
2022-07-04 20:05:08 +0530

    Time: 1801 ms (5.02%), Space: 149.2 MB (5.53%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int l = s.length(),i=0,j=0,res=0,SubstringLen=0;
-        unordered_map<char,int> mp;
-        //maintain 2 pointer, one to start of the substring and other to the end of substring
-        //initially both starts from 0th index
-        while(j<l){
-            //Check if the character exists in the map, if not then insert and increment count
-            //if exits in the map then get longest string length and reset count and starting index of substring
-            if(mp.find(s[j])!=mp.end()){
-                res = max(res, SubstringLen);
-                j=i;
-                i = mp[s[j]] + 1;
-                SubstringLen = 0;
-                mp.clear();
+        int l = s.size(),j=0;
+        int lls = 0,cnt=0;
+        unordered_map<char, int> unique;
+        for(int i=0;i<l;i++){
+            if(unique.find(s[i])!=unique.end()){
+                lls = max(lls, cnt);
+                i = j;
+                j = unique[s[i]] + 1;
+                unique.clear();
+                cnt = 0;    
+            }else{
+                unique[s[i]] = i;
+                cnt++;  
             }
-            else{
-                mp[s[j]] = j;
-                SubstringLen++;
-            }
-            j++;
         }
-        res = max(res,SubstringLen);
-        return res;
+        lls = max(lls, cnt);
+        return lls;
     }
 };
