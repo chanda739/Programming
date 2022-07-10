5eef92d56319ca65377bdf3851835bf959aec1f2
Chanda Gupta <chandaincse801@gmail.com>
2022-07-10 20:17:07 +0530

    Time: 113 ms (88.39%), Space: 33.7 MB (97.06%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        if(nums.size()<=1)return nums.size();
-       // unordered_set<int> s(nums.begin(),nums.end());
-        unordered_set<int> s;
-        for(int i : nums)s.insert(i);
-        int lcs = 0;
-        for(int i=0;i<nums.size();i++){
-            if(s.find(nums[i]-1)==s.end()){
-                int tmp = nums[i];
-                while(s.find(tmp)!=s.end())tmp++;
-                lcs = max(lcs, tmp - nums[i]);
-            }
+        int lcs = 1, j=1,l = nums.size(),i=0;
+        sort(nums.begin(),nums.end());
+        for(i=1;i<nums.size();i++){
+            if(nums[i]!=nums[i-1]){
+            if(nums[i]==nums[i-1]+1)j++;
+            else{
+                lcs = max(lcs, j);
+                j = 1;
+            }}
         }
-        return lcs;
+        lcs = max(lcs, j);
+        return l==0?0:lcs;
+        
+       //  if(nums.size()<=1)return nums.size();
+       // // unordered_set<int> s(nums.begin(),nums.end());
+       //  unordered_set<int> s;
+       //  for(int i : nums)s.insert(i);
+       //  int lcs = 0;
+       //  for(int i=0;i<nums.size();i++){
+       //      if(s.find(nums[i]-1)==s.end()){
+       //          int tmp = nums[i];
+       //          while(s.find(tmp)!=s.end())tmp++;
+       //          lcs = max(lcs, tmp - nums[i]);
+       //      }
+       //  }
+       //  return lcs;
         /*
         //TC: O(nlogn)
         sort(nums.begin(), nums.end());
