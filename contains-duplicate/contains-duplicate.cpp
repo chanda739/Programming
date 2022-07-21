dcc88924e4526117a2cd4420f8ee0cadf799d3e8
Chanda Gupta <chandaincse801@gmail.com>
2022-07-21 20:05:18 +0530

    Time: 161 ms, Memory: 53.4 MB - LeetHub

---
+++ contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> st;
+        for(auto x: nums)st.insert(x);
+        return st.size()!=nums.size();
+    }
+};
