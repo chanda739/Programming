4cb6a02b4b195433212888b650df5a682633cd5b
Chanda Gupta <chandaincse801@gmail.com>
2022-07-27 19:01:44 +0530

    Time: 17 ms (14.47%), Space: 12.8 MB (38.74%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
@@ -12,13 +12,17 @@
 class Solution {
 public:
     void flatten(TreeNode* root) {
+        //Base case
         if(root==nullptr)return;
+        
         TreeNode* left = root->left;
         TreeNode* right = root->right;
         root->left = nullptr;
-        
+        //Flatten left subtree
         flatten(left);
+        //Flatten right subtree
         flatten(right);
+        
         root->right = left;
         TreeNode* curr = root;
         while(curr->right!=nullptr)
@@ -26,37 +30,3 @@ public:
         curr->right = right;
     }
 };
-
-/*
-if(root==nullptr)return;
-        if(root->left==nullptr && root->right==nullptr)
-            return;
-        // if(root->left==nullptr)
-        //     flatten(root->right);
-        TreeNode* temp = root, *prev = nullptr;
-        while(temp->left!=nullptr){
-            prev = temp;
-            temp = temp->left;
-        }
-        while(temp->right!=nullptr){
-            temp = temp->right;
-        }
-        if(temp->right==nullptr){
-            if(prev!=nullptr){
-                temp->right = prev->right;
-                prev->right = nullptr;
-            }
-        }
-        while(temp->right!=nullptr){
-            temp = temp->right;
-        }
-        if(temp->right != root->right)
-            temp->right = root->right;
-        if(root->left!=nullptr)
-         {   root->right = root->left;
-            root->left = nullptr;
-         }
-        if(root->left==nullptr)
-            flatten(root->right);
-
-*/
