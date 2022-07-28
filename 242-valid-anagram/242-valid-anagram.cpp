b5fc9fc6193a29c1d42ecce5ea10204e4e9be0f8
Chanda Gupta <chandaincse801@gmail.com>
2022-07-28 19:22:03 +0530

    Time: 20 ms (32.51%), Space: 7.4 MB (45.59%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,10 +1,24 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size()!=t.size())
-            return false;
-        sort(s.begin(),s.end());
+        /*O(nlogn)
+        sort(s.begin(), s.end());
         sort(t.begin(), t.end());
-        return s==t;
+        return t==s;
+        */
+        /*O(n)*/
+        unordered_map<char, int> anagram;
+        for(char ch:s)anagram[ch]++;
+        
+        for(char ch:t){
+            if(anagram.find(ch)==anagram.end())return false;
+            else{
+                anagram[ch]--;
+                if(anagram[ch]==0){
+                    anagram.erase(ch);
+                }
+            }
+        }
+        return anagram.empty()?true:false;
     }
 };
