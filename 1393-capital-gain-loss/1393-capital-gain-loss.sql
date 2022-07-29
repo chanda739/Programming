8a1ad854bdc6f958c9b777987136dc9aa53ff9cf
Chanda Gupta <chandaincse801@gmail.com>
2022-07-29 22:51:52 +0530

    Time: 443 ms (87.06%), Space: 0B (100.00%) - LeetHub

---
+++ 1393-capital-gain-loss/1393-capital-gain-loss.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+select distinct stock_name, sum(case when operation = 'Buy' then -price else price end) 
+over (partition by stock_name) capital_gain_loss
+from stocks
+
