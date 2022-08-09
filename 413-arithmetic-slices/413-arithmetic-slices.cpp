d168005c15cbeaa434e1787e683c607f002a9ec2
Chanda Gupta <chandaincse801@gmail.com>
2022-08-09 16:13:57 +0530

    Time: 5 ms (33.26%), Space: 7.3 MB (50.60%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.cpp
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -5,6 +5,28 @@ public:
         if(n<3)return 0;
         int res = 0,count=0,diff=nums[1]-nums[0],curr;
         for(int i=1;i<n-1;i++){
+            //curr = nums[i+1]-nums[i];
+            //if(diff == curr){
+            if((nums[i]-nums[i-1]) == (nums[i+1]-nums[i])){
+                count++;
+            }else{
+               // diff = curr;
+                count = 0;
+            }
+            res+=count;
+        }
+        return res;
+    }
+};
+
+/*
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        int n = nums.size();
+        if(n<3)return 0;
+        int res = 0,count=0,diff=nums[1]-nums[0],curr;
+        for(int i=1;i<n-1;i++){
             curr = nums[i+1]-nums[i];
             if(diff == curr){
                 count++;
@@ -17,3 +39,4 @@ public:
         return res;
     }
 };
+**/
