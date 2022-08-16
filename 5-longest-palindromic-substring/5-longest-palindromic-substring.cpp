7df8036531ea3df67a22d1daf5e03751132a871f
Chanda Gupta <chandaincse801@gmail.com>
2022-08-16 07:20:19 +0530

    Time: 180 ms (57.03%), Space: 159.7 MB (23.74%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,38 +1,25 @@
 class Solution {
-    private:
-    void palindrome(string& s,int i,int j,vector<int>& res){
-        while(i>=0&&j<s.length()&&s[i]==s[j]){
-            j++;i--;
+    int isPalindrome(string s, int i, int j){
+        while(i>=0 && j<s.size() && s[i]==s[j]){
+                i--;j++;
         }
-        //cout<<"palindrome "<<i+1<<" "<<j-1<<endl;
-        res[0] = i+1;
-        res[1] = j-1;
+        return j - i - 1;
     }
-    public:
-    string longestPalindrome(string A) {
-        int l = A.length(),ans=INT_MIN,longestLen=0;
-        vector<int> res(2,0);vector<int> index(2,0);
-        for(int i=0;i<l;i++){
-            palindrome(A,i,i,res);
-            longestLen = res[1]-res[0]+1;
-            if(ans<longestLen){
-                ans = longestLen;
-                index[0] = res[0];
-                index[1] = res[1];
-            }
-            palindrome(A,i,i+1,res);
-            longestLen = res[1]-res[0]+1;        
-            if(ans<longestLen){
-                ans = longestLen;
-                index[0] = res[0];
-                index[1] = res[1];
+public:
+    string longestPalindrome(string s) {
+        if(s.size()<=1)return s;
+        int st = 0, end = 0;
+        
+        for(int i=0;i<s.size();++i){
+           int odd = isPalindrome(s,i,i);
+            int even = isPalindrome(s,i,i+1);
+            int len = max(odd, even);
+            if(len > (end-st)){
+                st = i - (len-1)/2;
+                end = i + (len)/2;
             }
         }
-        //cout<<"longestLen "<<ans;
-        string lps="";
-        for(int k =index[0];k<=index[1];k++)
-            lps+=A[k];
-        return lps;
+        int l = end-st+1;
+        return s.substr(st,l);
     }
-
 };
