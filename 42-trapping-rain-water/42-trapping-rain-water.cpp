a199a8c5a559727541b70676a69d4704a929c180
Chanda Gupta <chandaincse801@gmail.com>
2022-09-18 16:29:36 +0530

    Time: 33 ms (29.82%), Space: 20.5 MB (36.12%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -6,16 +6,18 @@ public:
         //generate Prefix Max array from left side
         for(i=0;i<l;i++){
             LeftMax[i]=height[i];
-            if(i!=0)LeftMax[i] = max(LeftMax[i-1],height[i]);
+            if(i!=0)
+                LeftMax[i] = max(LeftMax[i-1],height[i]);
         }
         //generate prefix max from right side
         for(i=l-1;i>=0;i--){
             RightMax[i]=height[i];
-            if(i!=(l-1))RightMax[i] = max(RightMax[i+1],height[i]);
+            if(i!=(l-1))
+                RightMax[i] = max(RightMax[i+1],height[i]);
         }
         int AmtWtrTrp=0,t;
         //first and last building won't trap water so skip them, iterate for rest of the building
-        //get max height from ⁵left and right side of the current building and get amount of water trapped
+        //get max height from left and right side of the current building and get amount of water trapped
         for(i=1;i<l-1;i++){
             t = min(LeftMax[i-1],RightMax[i+1]) - height[i];
             AmtWtrTrp+= max(0,t);
