20e0d64bb4f7186b1643e101e0a12e7e94c6cb41
Chanda Gupta <chandaincse801@gmail.com>
2022-11-07 22:10:26 +0530

    Time: 3 ms (38.22%), Space: 6 MB (17.66%) - LeetHub

---
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maximum69Number (int num) {
+        string n = to_string(num);
+        for(auto &ch:n){
+            if(ch=='6'){
+                ch='9';break;
+            }
+        }
+        return stoi(n);
+    }
+};
